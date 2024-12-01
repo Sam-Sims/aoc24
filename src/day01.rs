ed559a9e795942c4ab273ccd71eca2c2633f15b1
Sam Sims <samsimss98@gmail.com>
2024-12-01 15:08:54 +0000

    turns out preallocating col vectors was silly
    original approach was better as doesnt iterate the input twice

--- src/day01.rs
+++ src/day01.rs
@@ -9,23 +9,23 @@ use fxhash::FxHashMap;
 //             total_diff += num - col2[i];
 //         }
 //     }
-// 
+//
 //     println!("Part1: {}", total_diff);
 // }
-// 
+//
 // fn part2(col1: &[i32], col2: &[i32]) {
 //     let mut col2_counts: HashMap<i32, i32> = HashMap::new();
 //     for num in col2 {
 //         *col2_counts.entry(*num).or_insert(0) += 1;
 //     }
-// 
+//
 //     let mut total_diff = 0;
 //     for num in col1 {
 //         // since get() returns a reference, unwrap_or() also needs to return a reference (in this case to 0)
 //         let count = col2_counts.get(num).unwrap_or(&0);
 //         total_diff += *num * count;
 //     }
-// 
+//
 //     println!("Part2: {}", total_diff);
 // }
 
@@ -47,7 +47,7 @@ fn part2_improved(col1: &[i32], col2: &[i32]) {
     for num in col2 {
         *col2_counts.entry(*num).or_insert(0) += 1;
     }
-    
+
     // again making better use of iterator methods
     let total_diff: i32 = col1.iter()
         .map(|num| num * col2_counts.get(num).unwrap_or(&0))
@@ -57,19 +57,16 @@ fn part2_improved(col1: &[i32], col2: &[i32]) {
 }
 
 pub fn run(input: &str) {
-    // a bit more verbose but allows you to preallocate memory in the vectors
-    let line_count = input.lines().count();
-    let mut col1: Vec<i32> = Vec::with_capacity(line_count);
-    let mut col2: Vec<i32> = Vec::with_capacity(line_count);
-
-    for line in input.lines() {
-        let nums: Vec<i32> = line
-            .split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect();
-        col1.push(nums[0]);
-        col2.push(nums[1]);
-    }
+    let (mut col1, mut col2): (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .map(|line| {
+            let nums: Vec<i32> = line
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect();
+            (nums[0], nums[1])
+        })
+        .unzip();
 
     // we can use sort_unstable as the order of equal elements doesnt matter, and apparently its faster
     col1.sort_unstable();
