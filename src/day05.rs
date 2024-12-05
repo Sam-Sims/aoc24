8577af728a560b950b56a932169ff5f8ab19af6e
Sam Sims <samsimss98@gmail.com>
2024-12-05 18:04:55 +0000

    day05 improved

--- src/day05.rs
+++ src/day05.rs
@@ -1,32 +1,54 @@
 use std::collections::HashMap;
 
+// fn part1(rules_map: &HashMap<i32, Vec<i32>>, instructions: &str) -> (i32, Vec<Vec<i32>>) {
+//     let mut passed_instruction_sets: Vec<Vec<i32>> = Vec::new();
+//     let mut failed_instruction_sets: Vec<Vec<i32>> = Vec::new();
+//
+//     // loop through every instruction
+//     for instruction in instructions.lines() {
+//         let instructions_vec: Vec<i32> =
+//             instruction.split(",").map(|i| i.parse().unwrap()).collect();
+//
+//         let mut pass = true;
+//         // for each number in the instructions vector look up its set of rules in the hashmap
+//         for i in 0..instructions_vec.len() {
+//             let Some(rules) = rules_map.get(&instructions_vec[i]) else {
+//                 continue;
+//             };
+//             // check each rule returned from the lookup
+//             // the current index must be behind the index of the identifed rules value
+//             // if it isnt then the instruction doesnt pass
+//             for rule in rules.iter() {
+//                 if let Some(rule_index) = instructions_vec.iter().position(|&i| i == *rule) {
+//                     if rule_index < i {
+//                         pass = false;
+//                     }
+//                 }
+//             }
+//         }
+//         if pass {
+//             passed_instruction_sets.push(instructions_vec);
+//         } else {
+//             failed_instruction_sets.push(instructions_vec);
+//         }
+//     }
+//
+//     (
+//         passed_instruction_sets.iter().map(|i| i[i.len() / 2]).sum(),
+//         failed_instruction_sets,
+//     )
+// }
+
+// an improved part1 after someone on reddit suggested the is_sorted_by() function
 fn part1(rules_map: &HashMap<i32, Vec<i32>>, instructions: &str) -> (i32, Vec<Vec<i32>>) {
     let mut passed_instruction_sets: Vec<Vec<i32>> = Vec::new();
     let mut failed_instruction_sets: Vec<Vec<i32>> = Vec::new();
-
-    // loop through every instruction
     for instruction in instructions.lines() {
         let instructions_vec: Vec<i32> =
             instruction.split(",").map(|i| i.parse().unwrap()).collect();
-
-        let mut pass = true;
-        // for each number in the instructions vector look up its set of rules in the hashmap
-        for i in 0..instructions_vec.len() {
-            let Some(rules) = rules_map.get(&instructions_vec[i]) else {
-                continue;
-            };
-            // check each rule returned from the lookup
-            // the current index must be behind the index of the identifed rules value
-            // if it isnt then the instruction doesnt pass
-            for rule in rules.iter() {
-                if let Some(rule_index) = instructions_vec.iter().position(|&i| i == *rule) {
-                    if rule_index < i {
-                        pass = false;
-                    }
-                }
-            }
-        }
-        if pass {
+        // nice function that checks each pair of values in the vector, if the rules map for A contains its adjacent value (B) then A is allowed to be before B
+        // this will be then compare each pair in the vec and return true if they all pass
+        if instructions_vec.is_sorted_by(|a, b| rules_map[a].contains(b)) {
             passed_instruction_sets.push(instructions_vec);
         } else {
             failed_instruction_sets.push(instructions_vec);
@@ -50,8 +72,8 @@ fn part2(failed_instructions: &mut Vec<Vec<i32>>, rules_map: &HashMap<i32, Vec<i
                 if instructions.contains(rule) {
                     let rule_index = instructions.iter().position(|&i| i == *rule).unwrap();
                     if rule_index < i {
-                        // however if we find an instruction failing the rule, swap them
-                        // (we brute force it by running this 50 times lol, so no "pass" condition
+                        // if we find an instruction failing the rule, swap them
+                        // we brute force it by running this 50 times lol, so no "pass" condition
                         // wait is this bubble sort??
                         instructions.swap(i, rule_index);
                     }
